e1b6bca497e28d0232ba703d50055dbce8f8959f
Sandeep <bnf369o111@gmail.com>
2026-06-16 15:56:22 +0530

    dkdfkjdf

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const path = require('path');
-
+const fs = require('fs');
 
 // is baar nahi hua to kya hoga?
 
@@ -8,9 +8,17 @@ const hostname = '0.0.0.0';
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.sendFile(path.join(__dirname, 'index.html'));
+  const filePath = path.join(__dirname, 'index.html');
+
+  fs.readFile(filePath, 'utf8', (err, html) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      return res.end('Unable to load index.html');
+    }
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(html);
+  });
 });
 
 server.listen(port, hostname, () => {
